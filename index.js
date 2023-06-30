afb57525a9652e8721ecceca04ee3596536a0910
Kodebube <137894304+Kodebube@users.noreply.github.com>
2023-06-30 14:25:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+let close = document.getElementById("links")
+
+document.getElementById("showmenu").onclick = function(){
+    links.style.left = "0"
+}
+
+document.getElementById("hidemenu").onclick = function(){
+    links.style.left = "-300px"
+}*/
+
+//----------OR----------//
+
+function hidemenu(){
+    links.style.left = "-300px"
+}
+
+function showmenu(){
+    links.style.left = "0"
+}
